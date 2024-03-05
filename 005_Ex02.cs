19d1292da21450554a8ef4dd984b86a914aac7dd
SimpleHarmonicMotion <hmshin070209@gmail.com>
2024-03-05 09:00:41 +0900

    2024/03/05

---
+++ 005_Ex02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ex02
+{
+    class Program{
+        static void Main(string[] args){
+            string s = "C# Studies";
+
+            for( int i = 0 ; i < s.Length ; i++){
+                Console.WriteLine("{0}: {1}", i, s[i]);
+            }
+
+            string str = "Hello";
+
+            char[] charArray = str.ToCharArray();
+
+            for( int i = 0 ; i < charArray.Length ; i++ ){
+                Console.WriteLine(charArray[i]);
+            }
+
+            char[] charArray2 = { 'A', 'B', 'C', 'D' };
+            s = new string(charArray2);
+
+             Console.WriteLine(s);
+
+             char c1 = 'A';
+             char c2 = (char)(c1 + 3);
+             Console.WriteLine(c2);
+        }
+    }
+}
---
+++ 005_Ex03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            for( int i = 1 ; i <= 26 ; i++ )
+            {
+                sb.Append(i.ToString());
+                sb.Append(System.Environment.NewLine);
+            }
+            string s = sb.ToString();
+            Console.WriteLine(s);
+        }
+    }
+}
